458bd5b1bbdd1275265b1c4f827dedee93cd7583
ayesha shaheen <ayesha.school@hotmail.com>
2021-08-11 12:28:05 -0400

    adding content

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_pymongo import PyMongo
 import scraping
 
 app = Flask(__name__)
+#app=Flask(__name__,template_folder='Templates')
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
@@ -21,4 +22,4 @@ def scrape():
    return redirect('/', code=302)
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
